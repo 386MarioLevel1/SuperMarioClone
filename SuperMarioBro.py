d994b4cf287023facdadf8c4c69d30f7ec7ba84b
WellsonPan <dihydrogenmonoxide1337@gmail.com>
2018-11-04 16:15:55 -0800

    forgot to update mario

--- SuperMarioBro.py
+++ SuperMarioBro.py
@@ -21,8 +21,9 @@ def run_game():
 
     # Start the main loop for the game
     while True:
-        gf.check_events(ai_settings, screen)
+        gf.check_events(ai_settings, screen, mario)
         map.update()
+        mario.update()
         gf.update_screen(ai_settings, screen, mario, map)
 
 
